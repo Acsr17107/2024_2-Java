748e0e724a84f3c797c6e2028c021cb0fd25ac78
Neon <sj060700@naver.com>
2024-12-06 12:00:25 +0900

    Delete ch10/my_module_b directory

--- ch10/my_module_b/src/pack3/C.java
+++
@@ -1,8 +0,0 @@
-package pack3;
-
-public class C {
-	//메소드 선언
-	public void method() {
-		System.out.println("C-method 실행");	
-	}
-}
--- ch10/my_module_b/src/pack4/D.java
+++
@@ -1,8 +0,0 @@
-package pack4;
-
-public class D {
-	//메소드 선언
-	public void method() {
-		System.out.println("D-method 실행");	
-	}
-}
